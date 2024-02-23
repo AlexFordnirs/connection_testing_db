d069395e05070979b5c986b45aa96afcbad55d4f
AlexFordnirs <71636706+AlexFordnirs@users.noreply.github.com>
2024-02-23 17:18:33 +0200

    Add files via upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        
+        //Initilize elements
+        public static Label StatusLabel;
+        public static Label ServerLabel;
+        public static Label DBLabel;
+        public static Label TableLabel;
+        public static TextBox ServerNameTb;
+        public static TextBox ComandTb;
+        public static ComboBox DbCb;
+        public static ComboBox TableCb;
+        public static Button ConnectBtn;
+        public static Button TestBtn;
+        public static Button DoBtn;
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // Form1
+            this.Size = new Size(400, 400);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Form1";
+            this.Text = "Form1";
+
+            this.ResumeLayout(false);
+
+            //ServerLabel create 
+            ServerLabel = new Label();
+            ServerLabel.Text = "Server name:";
+            ServerLabel.Location = new Point(10, 10);
+            //DbLabel create
+            DBLabel = new Label();
+            DBLabel.Text = "Data base:";
+            DBLabel.Location = new Point(10, 50);
+            //TadleLabel create
+            TableLabel = new Label();
+            TableLabel.Text = "Table:";
+            TableLabel.Location = new Point(10, 90);
+            //StatusLabel create
+            StatusLabel = new Label();
+            StatusLabel.Text = "Status:\r\n";
+            StatusLabel.Location = new Point(300, 290);
+            StatusLabel.Size = new Size(60, 60);
+            //ServerNameTb create
+            ServerNameTb = new TextBox();
+            ServerNameTb.Text = "Server1";
+            ServerNameTb.Size = new Size(200, 50);
+            ServerNameTb.Location = new Point(110, 10);
+            ServerNameTb.MaxLength = 25;
+            //ComandTb create
+            ComandTb = new TextBox();
+            ComandTb.Size = new Size(200, 200);
+            ComandTb.Multiline = true;
+            ComandTb.Location = new Point(20, 150);
+            //ConnectBtn create
+            ConnectBtn = new Button();
+            ConnectBtn.Text = "Connect";
+            ConnectBtn.Size = new Size(60, 20);
+            ConnectBtn.Location = new Point(320, 10);
+            ConnectBtn.Click += ConnectBtn_click;
+            //TestBtn create
+            TestBtn = new Button();
+            TestBtn.Text = "Test";
+            TestBtn.Size = new Size(60, 20);
+            TestBtn.Location = new Point(290, 220);
+            //TestBtn.Click += testBtn_click;
+            //DoBtn create
+            DoBtn = new Button();
+            DoBtn.Text = "Do";
+            DoBtn.Size = new Size(60, 20);
+            DoBtn.Location = new Point(290, 250);
+            DoBtn.Click += DoBtn_Click;
+            //DbCb
+            DbCb = new ComboBox();
+            DbCb.Size = new Size(200, 20);
+            DbCb.Location = new Point(110, 50);
+            DbCb.SelectedIndexChanged += DbCb_SelectedIndexChanged;
+            //TableCb
+            TableCb = new ComboBox();
+            TableCb.Size = new Size(200, 20);
+            TableCb.Location = new Point(110, 90);
+            TableCb.SelectedIndexChanged += TableCb_SelectedIndexChanged;
+            //Add to Form1
+            this.Controls.Add(ServerLabel);
+            this.Controls.Add(DBLabel);
+            this.Controls.Add(TableLabel);
+            this.Controls.Add(StatusLabel);
+            this.Controls.Add(ServerNameTb);
+            this.Controls.Add(ComandTb);
+            this.Controls.Add(ConnectBtn);
+            this.Controls.Add(TestBtn);
+            this.Controls.Add(DoBtn);
+            this.Controls.Add(DbCb);
+            this.Controls.Add(TableCb);
+        }
+
+        
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        private static string Conn = "";
+        private static SqlConnection sqlConnection = new SqlConnection();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ConnectBtn_click(object sender, EventArgs e)
+        {
+            if (ServerNameTb.TextLength == 0)
+            {
+                MessageBox.Show("Connection string can not de empty!");
+                return;
+            }
+            try
+            {
+                Conn = "Data Source=" + ServerNameTb.Text + ";User ID=Student;Password=;Integrated Security=False;";
+                if (sqlConnection.DataSource != ServerNameTb.Text)
+                {
+                    sqlConnection.ConnectionString = Conn;
+                    sqlConnection.Open();
+                }
+                StatusLabel.Text = "Status: \nConnected";
+                DbCb.Enabled = true;
+                GetDBs();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.Message.ToString());
+                return;
+            }
+        }
+
+        public void GetDBs()
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", sqlConnection))
+                {
+                    using (IDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            DbCb.Items.Add(dr[0].ToString());
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.Message);
+                return;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DbCb.Enabled = false;
+            TableCb.Enabled = false;
+            ComandTb.Enabled = false;
+            ConnectBtn.Enabled = false;
+            TestBtn.Enabled = false;
+            StatusLabel.Text = "Status: No \nconnection";
+        }
+
+        private void DbCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TableCb.Enabled = true;
+            using (SqlCommand cmd = new SqlCommand("USE " + DbCb.SelectedItem.ToString() + "; SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES", sqlConnection))
+            {
+                using (IDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        TableCb.Items.Add(dr[0].ToString());
+                    }
+                }
+            }
+        }
+        private void TableCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComandTb.Enabled = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (ComandTb.TextLength < 1)
+            {
+                TestBtn.Enabled = false;
+                DoBtn.Enabled = false;
+            }
+            else
+            {
+                TestBtn.Enabled = true;
+                DoBtn.Enabled = true;
+            }
+        }
+
+        private void DoBtn_Click(object sender, EventArgs e)
+        {
+            if (sqlConnection.State.ToString() == "Closed")
+            {
+                sqlConnection.Open();
+            }
+            try
+            {
+                SqlCommand cmd = new SqlCommand("USE " + DbCb.SelectedItem.ToString() + "; " + ComandTb.Text + " [" + TableCb.Text + "];", sqlConnection);
+                cmd.ExecuteNonQuery();
+                StatusLabel.Text = "Status: \nLast OK";
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                StatusLabel.Text = "Status: \nLast NO";
+            }
+        }
+    }
+}
